f5edc49581fbfe702de35cf8852f01b1789d90ce
dwl21 <nggus5@naver.com>
2022-11-17 16:48:24 +0900

    refactor: 다리 이동 가능 여부 메서드 분리

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -24,12 +24,16 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public BridgeGame move(final Space movingSpace) {
-        Space space = Space.valueOf(spaces.get(position.getValue()));
-        if (space.equals(movingSpace)) {
+    public BridgeGame move(final Space space) {
+        if (canMove(space)) {
             return new BridgeGame(position.nextPosition(), this.spaces);
         }
         return this;
+    }
+
+    private boolean canMove(final Space space) {
+        Space now = Space.valueOf(spaces.get(position.getValue()));
+        return now.equals(space);
     }
 
     /**
